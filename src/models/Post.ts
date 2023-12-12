2331066c34750653177985c773c963c910daf293
CyberGalactus <edu.garma97@hotmail.com>
2023-12-12 14:59:39 +0100

    commit

--- src/models/Post.ts
+++ src/models/Post.ts
@@ -1,2 +1,33 @@
+import { Schema } from "mongoose";
 
+interface IPost extends Document {
+    title: string;
+    link?: string;
+    body?: string;
+    author: Types.ObjectId;
+    updatedAt: Date;
+}
+
+const PostSchema = new Schema<Post> ({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    link: {
+        type: String,
+        trim: true
+    },
+    body: {
+        type: String,
+    },
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+
+    }, 
+    
+
+});
 
